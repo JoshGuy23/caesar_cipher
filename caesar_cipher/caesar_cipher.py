ac597f3f9c6d2a3932430077bb10aded566bb3fc
Joshua Hecker <jhecker2001@gmail.com>
2023-10-16 09:43:08 -0500

    Combined encrypt and decrypt functions into a single function.

--- caesar_cipher/caesar_cipher.py
+++ caesar_cipher/caesar_cipher.py
@@ -3,26 +3,21 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
-
-def encrypt(plain_text, shift_amount):
+    
+def caesar(input_text, shift_amount, text_direction):
     new_text = ""
-    for c in plain_text:
-        new_index = alphabet.index(c) + shift_amount
-        while new_index > 25:
-            new_index -= 26
+    for c in input_text:
+        if text_direction == "encode":
+            new_index = alphabet.index(c) + shift_amount
+            while new_index > 25:
+                new_index -= 26
+        elif text_direction == "decode":
+            new_index = alphabet.index(c) - shift_amount
+            while new_index < 0:
+                new_index += 26
+        else:
+            break
         new_text += alphabet[new_index]
-    print(f"The encoded text is {new_text}.")
+    print(f"The new text is {new_text}.")
 
-def decrypt(cipher_text, shift_amount):
-    new_text = ""
-    for c in cipher_text:
-        new_index = alphabet.index(c) - shift_amount
-        while new_index < 0:
-            new_index += 26
-        new_text += alphabet[new_index]
-    print(f"The decoded text is {new_text}.")
-
-if direction == "encode":    
-    encrypt(text, shift)
-elif direction == "decode":
-    decrypt(text, shift)
+caesar(text, shift, direction)
