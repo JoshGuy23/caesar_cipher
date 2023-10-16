a5e70b0e0e91bba8893681f333905c0c9397a5f2
Joshua Hecker <jhecker2001@gmail.com>
2023-10-16 09:34:32 -0500

    Added some quality of life features. The input for the direction is now always lowercase, and the functions have been adjusted for unusually large shift amounts.

--- caesar_cipher/caesar_cipher.py
+++ caesar_cipher/caesar_cipher.py
@@ -1,6 +1,6 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
@@ -8,7 +8,7 @@ def encrypt(plain_text, shift_amount):
     new_text = ""
     for c in plain_text:
         new_index = alphabet.index(c) + shift_amount
-        if new_index > 25:
+        while new_index > 25:
             new_index -= 26
         new_text += alphabet[new_index]
     print(f"The encoded text is {new_text}.")
@@ -17,7 +17,7 @@ def decrypt(plain_text, shift_amount):
     new_text = ""
     for c in plain_text:
         new_index = alphabet.index(c) - shift_amount
-        if new_index < 0:
+        while new_index < 0:
             new_index += 26
         new_text += alphabet[new_index]
     print(f"The decoded text is {new_text}.")
