4b14de965477bcd2ec77dbf82f470931bbfd6706
Joshua Hecker <jhecker2001@gmail.com>
2023-10-16 09:18:06 -0500

    Finished encryption function.

--- caesar_cipher/caesar_cipher.py
+++ caesar_cipher/caesar_cipher.py
@@ -5,4 +5,10 @@ text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 def encrypt(text, shift):
-    
+    new_text = ""
+    for c in text:
+        new_index = alphabet.index(c) + shift
+        if new_index > 25:
+            new_index -= 26
+        new_text += alphabet.index(new_index)
+    print(new_text)
